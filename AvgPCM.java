5f4b4aea1e7bb9bb09a972b81aeff1dad1ddfcea
Khyati Sharma <skhyati904@gmail.com>
2026-02-23 14:10:10 +0530

    week1and2 added

---
+++ AvgPCM.java
@@ -0,0 +1,9 @@
+public class AvgPCM{
+	public static void main(String[] args){
+		int m=94;
+		int p=95;
+		int c=96;
+		double avg=(p+c+m)/3.0;
+		System.out.println("Sam's average mark in PCM is " + avg);
+	}
+}
---
+++ week1and2.java
@@ -0,0 +1,8 @@
+public class HarryAge{
+	public static void main(String args[]){
+		int birthyear=2000;
+		int currentyear=2024;
+		int age=currentyear-birthyear;
+		System.out.printlln("Harry's age is "+age);
+	}
+}
