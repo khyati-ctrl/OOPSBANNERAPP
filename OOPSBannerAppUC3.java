c5c2db90254d639acc9dfb7e01aee122e261bfc4
Khyati Sharma <skhyati904@gmail.com>
2026-02-23 12:54:58 +0530

    UC3 Added

---
+++ OOPSBannerAppUC3.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerApp UC3 - OOPS Banner Application
+ * Using String.join() for memory efficiency.
+ */
+public class OOPSBannerAppUC3 {
+
+    public static void main(String[] args) {
+        System.out.println(String.join("  ", "  *** ", "  ***  ", "***** ", " ****"));
+        System.out.println(String.join("  ", "**   **", "**  **", "**  **", "**   "));
+        System.out.println(String.join("  ", "**   **", "**  **", "**  **", "**   "));
+        System.out.println(String.join("  ", "**   **", "**  **", "******", " *** "));
+        System.out.println(String.join("  ", "**   **", "**  **", "** ", "      **"));
+        System.out.println(String.join("  ", "**   **", "**  **", "** ", "      **"));
+        System.out.println(String.join("  ", "  *** ", "  *** ", " ** ", "  **** "));
+    }
+}
