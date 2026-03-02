23a962ebfd7213c7b7108277521a5da7db12ab5f
Khyati Sharma <skhyati904@gmail.com>
2026-03-02 14:09:06 +0530

    Delete week1and2.java

--- week1and2.java
+++
@@ -1,8 +0,0 @@
-public class HarryAge{
-	public static void main(String args[]){
-		int birthyear=2000;
-		int currentyear=2024;
-		int age=currentyear-birthyear;
-		System.out.printlln("Harry's age is "+age);
-	}
-}
